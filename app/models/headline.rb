a83132936d05240a196101d080b0f4ddc6d9c786
craig <cwn720@gmail.com>
2015-07-07 08:55:08 -0700

    develop headline model using tdd

--- app/models/headline.rb
+++ app/models/headline.rb
@@ -1,3 +1,5 @@
 class Headline < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, presence: true, length: { minimum: 10, maximum: 255 }
+  validates :ref, presence: true, :format => URI::regexp(%w(http https))
+  validates :time, presence: true
 end
--- spec/models/headline_spec.rb
+++ spec/models/headline_spec.rb
@@ -9,7 +9,43 @@ RSpec.describe Headline, :type => :model do
   it 'has a title' do
     @headline.title = ''
 
-    expect(@headline.valid?).to be(FALSE)
+    expect(@headline.valid?).to be(false)
+  end
+
+  it 'title is at least 10 characters long' do
+    @headline.title = 'a'*5
+    expect(@headline.valid?).to be(false)
+    @headline.title = 'a'*10
+    expect(@headline.valid?).to be(true)
+  end
+
+  it 'title should be no more than 255 characters long' do
+    @headline.title = 'a'*255
+    expect(@headline.valid?).to be(true)
+    @headline.title = 'a'*256
+    expect(@headline.valid?).to be(false)
+  end
+
+  it 'has a ref link' do
+    @headline.ref = ''
+    expect(@headline.valid?).to be(false)
+  end
+
+  it 'has a ref link with a valid format' do
+    @headline.ref = 'http://www.google.com'
+    expect(@headline.valid?).to be(true)
+
+    @headline.ref = 'this is not a link'
+    expect(@headline.valid?).to be(false)
+
+  end
+
+  it 'has a time' do
+    @headline.time = 0
+    expect(@headline.valid?).to be(false)
+
+    @headline.time = Time.now
+    expect(@headline.valid?).to be(true)
   end
 
 end
